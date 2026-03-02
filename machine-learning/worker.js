511b5382952931821538882daff47554d63a0722
Rafael <cast.rafael@sebraego.com.br>
2026-03-02 14:01:42 -0300

    usando modelo no woker, iniciando desenvolvimeno, minuto 04:08

--- machine-learning/worker.js
+++ machine-learning/worker.js
@@ -3,6 +3,26 @@ importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
 
+let _labels = []
+let _model = null
+
+async function loadModelAndLabels() {
+    await tf.ready()
+
+    _labels = await ( await fetch(LABELS_PATH)).json()
+    _model = await tf.loadGraphModel(MODEL_PATH)
+
+    // warmup
+    const dummyInput = tf.ones(_model.inputs[0].shape)
+    await _model.executeAsync(dummyInput)
+    tf.dispose(dummyInput)
+
+    postMessage({
+        type: 'modelLoaded',
+    })
+}
+
+loadModelAndLabels()
 
 self.onmessage = async ({ data }) => {
     if (data.type !== 'predict') return
